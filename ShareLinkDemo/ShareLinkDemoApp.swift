8f3c457d9685d26929a5a75145518272935afc49
Rheaparks <contact@rheaparks.com>
2022-11-08 15:34:23 +0100

    Initial Commit

---
+++ ShareLinkDemo/ShareLinkDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareLinkDemoApp.swift
+//  ShareLinkDemo
+//
+//  Created by Clément Cardonnel on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShareLinkDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
